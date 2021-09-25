23bd59001832c9a1d9d6681017b0cb3cfa6b1088
whynotnam <90708295+whynotnam@users.noreply.github.com>
2021-09-25 23:46:17 +0900

    Add files via upload

---
+++ [11399] ATM.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    
+    int N;                  // N = 사람의 수
+    scanf("%d", &N);    // N 입력
+    
+    int i;
+    for ( i = 0; i < N; i++) {
+    scanf("%d", &i);
+   }
+
+   int P;
+    for ( i = 0; i < N; i++ ) {
+	P = P + i
+  }
+  
+ print("%d", P);
+}
---
+++ [11399] ATM.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;   // N, K 입력
+    
+   int i;
+   for ( i = 0; i < N; i++) {
+   cin >> i; 
+  }
+
+    int P = 0; 
+    for (i = 0; i < N; i++) {
+           
+           P = P + i;
+        }
+
+    print("%d", P);
+}
