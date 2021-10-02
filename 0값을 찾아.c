607f73774325126af54d426a4000268b9b10b160
whynotnam <90708295+whynotnam@users.noreply.github.com>
2021-10-02 23:50:51 +0900

    Update 0값을 찾아.c

--- 0값을 찾아.c
+++ 0값을 찾아.c
@@ -2,13 +2,15 @@
 
 int main() {
     
-	int i,n;
+	int i,n,j;
 
 	scanf("%d", &n);	// 입력
 	int arr1[n];
 
 	for (i=0; i<n; i++)  {
-	scanf("%d", &arr1[i]); 
+	scanf("%d", &arr1[i]);
+	arr1[j] = arr1[i];
+	j = j + 1;
 	}
 
 ​	int n = sizeof(arr1) / sizeof(arr1[0]);
