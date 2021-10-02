b111218e15d8bddc4109366b579903446966e390
whynotnam <90708295+whynotnam@users.noreply.github.com>
2021-10-02 23:50:05 +0900

    Update 0값을 찾아.cpp

--- 0값을 찾아.cpp
+++ 0값을 찾아.cpp
@@ -2,18 +2,19 @@
 using namespace std;
 
 int main() {
-    int i,n;
+    int i,n,j;
     cin >> i;   // 입력
     int arr1[n];    
 
    for ( i = 0; i < n; i++) {
    cin >> i;
-   cout << arr1[n]; 
+   arr1[j] = arr1[i];
+   cout << arr1[n];
   }
 
 ​   int n = sizeof(arr1) / sizeof(arr1[0]);
    int arr2[n];
-   int j = 0;
+
 
    for (int i = j; i < n; i++) {
    arr2[i] = 0;
